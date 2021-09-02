03049490ec074008dc98849c3fa65c7351ff82c3
mariavaghani <maria.vaghani@gmail.com>
2021-09-02 14:25:07 -0400

    Add two_sum and my_transpose methods and specs

--- towers_of_hanoi/lib/test.rb
+++ towers_of_hanoi/lib/test.rb
@@ -4,4 +4,15 @@ class Array
         self.each { |ele| uniq_r << ele unless uniq_r.include?(ele)}
         uniq_r
     end
+
+
+    def two_sum
+        positions = []
+        (0...self.length).each do |first_idx|
+        (first_idx + 1...self.length).each do |second_idx|
+            positions << [first_idx, second_idx] if self[first_idx] + self[second_idx] == 0
+        end
+        end
+        positions
+    end
 end
--- towers_of_hanoi/spec/test_spec.rb
+++ towers_of_hanoi/spec/test_spec.rb
@@ -1,10 +1,39 @@
 require "test"
 describe Array do
     subject(:array) {[1, 2, 1, 3, 3]}
+    subject(:array_sum) {[-1, 0, 2, -2, 1]}
+
     describe "#my_uniq" do
         it "removes duplicates from an array" do
             expect(array.my_uniq).to eq([1, 2, 3])
         end
 
     end
+
+    describe "#two_sum" do
+        it "finds all pairs of positions where the elements at those positions sum to zero" do
+            expect(array_sum.two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        it "return an empty array when no pair is found" do
+            expect(array.two_sum).to eq([])
+
+        end
+
+    end
+
+    
+    
+    describe "#my_transpose" do
+        it "will convert between the row-oriented and column-oriented representations" do
+            test1_in = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+            test1_out = [[0, 3, 6],[1, 4, 7],[2, 5, 8]]
+            test2_in = [[0, 1, 2],[3, 4, 5],[6, 7, 8]]
+            test2_out = [[0, 3, 6],[1, 4, 7],[2, 5, 8]]
+            
+        end
+    end
+
+
+
 end
